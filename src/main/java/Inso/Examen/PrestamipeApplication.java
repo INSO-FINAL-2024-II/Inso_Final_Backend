5fe46a43c63b8258b20ce81dddca70a4e95e9e25
FluidityCore <lmostaceroc1@upao.edu.pe>
2024-12-01 11:12:57 -0500

    Estructura del proyecto

---
+++ src/main/java/Inso/Examen/PrestamipeApplication.java
@@ -0,0 +1,13 @@
+package Inso.Examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrestamipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrestamipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Inso/Examen/PrestamipeApplicationTests.java
@@ -0,0 +1,13 @@
+package Inso.Examen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestamipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
