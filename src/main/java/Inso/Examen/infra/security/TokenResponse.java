6f442a371ab6a4269077507c12b61ee80691863f
FluidityCore <lmostaceroc1@upao.edu.pe>
2024-12-01 11:54:04 -0500

    Estructura del proyecto con carpetas

---
+++ src/main/java/Inso/Examen/infra/security/TokenResponse.java
@@ -0,0 +1,14 @@
+package Inso.Examen.infra.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenResponse {
+    String token;
+}
